185c06aa88625ece3f8990cdfffa34ff62339df3
190330249 <190330249@klh.edu.in>
2021-03-03 08:45:49 +0530

    commit

--- venv/skill3delete.py
+++ venv/skill3delete.py
@@ -1,5 +1,5 @@
 import mysql.connector as mysql
 mydb=mysql.connect(host="localhost",user="root",password="varudhini1979",database="Hospital")
 mycursor=mydb.cursor()
-mycursor.execute("Update patient SET bill='Rs.1,10,000/-' WHERE Patient_id=1004")
+mycursor.execute("Delete from patient where Patient_id=1011")
 mydb.commit()
--- venv/skill3read.py
+++ venv/skill3read.py
@@ -1,5 +1,6 @@
 import mysql.connector as mysql
 mydb=mysql.connect(host="localhost",user="root",password="varudhini1979",database="Hospital")
 mycursor=mydb.cursor()
-mycursor.execute("Update patient SET bill='Rs.1,10,000/-' WHERE Patient_id=1004")
-mydb.commit()
+mycursor.execute("select * from medicine")
+for db in mycursor:
+    print(db)
