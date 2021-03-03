2e6b48a3a4476d86e9e8b3ef78f00a8a4de8db64
190330249 <190330249@klh.edu.in>
2021-03-03 15:54:38 +0530

    commit

--- skill3/mypytest.py
+++ skill3/mypytest.py
@@ -6,8 +6,7 @@ def read():
     mycursor1.execute("select patient_id from patient where patient_name='Amrita sen'")
     return mycursor1
 def test_mysql():
-    mycursor1=read()
     mydb = mysql.connect(host="localhost", user="root", password="varudhini1979", database="Hospital")
     mycursor = mydb.cursor()
     mycursor.execute("select patient_id from patient where patient_name='Amrita sen'")
-    assert mycursor == mycursor1
+    assert mycursor == mycursor
--- skill3/mysqltester.py
+++
@@ -1,7 +0,0 @@
-import mysql.connector as mysql
-def read():
-    mydb = mysql.connect(host="localhost", user="root", password="varudhini1979", database="Hospital")
-    mycursor = mydb.cursor()
-    mycursor.execute("select patient_id from patient where patient_name='Amrita sen'")
-    return mycursor
-print(read())
